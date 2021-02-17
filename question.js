5c37779061e4304b2da3f7027f8182f0cc7a99f1
Sammy <76911140+Sam-123-tech@users.noreply.github.com>
2021-02-16 23:31:12 -0800

    Add files via upload

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which planet is called the red planet ?",
+    answer: "Mars",
+    options: [
+      "Mars",
+      "Jupiter",
+      "Venus",
+      "Mercury"
+    ]
+  },
+    { 
+    numb: 2,
+    question: "What is the largest planet of the solar system ?",
+    answer: "Jupiter",
+    options: [
+      "Jupiter",
+      "Mars",
+      "Uranus",
+      "Saturn"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which planet is called the windy planet ?",
+    answer: "Neptune",
+    options: [
+      "Jupiter",
+      "Neptune",
+      "Saturn",
+      "Uranus"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which planet is calle the blue planet ?",
+    answer: "Earth",
+    options: [
+      "Uranus",
+      "Neptune",
+      "Earth",
+      "Mercury"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which planet is the smallest planet of the solar system ?",
+    answer: "Mercury",
+    options: [
+      "Mars",
+      "Venus",
+      "Jupiter",
+      "Mercury"
+    ]
+  },
+]; 
